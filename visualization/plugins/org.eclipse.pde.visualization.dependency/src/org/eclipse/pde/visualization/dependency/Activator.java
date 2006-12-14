2d6d97f1049c28ed04988956d2d54fde09479c37
ibull <>
2006-12-14 07:21:12 +0000

    add license files

--- visualization/plugins/org.eclipse.pde.visualization.dependency/src/org/eclipse/pde/visualization/dependency/Activator.java
+++ visualization/plugins/org.eclipse.pde.visualization.dependency/src/org/eclipse/pde/visualization/dependency/Activator.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright 2005-2006, CHISEL Group, University of Victoria, Victoria, BC, Canada.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     The Chisel Group, University of Victoria
+ *     IBM CAS, IBM Toronto Lab
+ *******************************************************************************/
 package org.eclipse.pde.visualization.dependency;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
