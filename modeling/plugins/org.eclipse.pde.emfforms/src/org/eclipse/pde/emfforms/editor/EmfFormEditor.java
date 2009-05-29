d5eb0cd5906fb0cc1089ddae3bcec8ec4f77a5cc
Benjamin Cabe <bcabe>
2009-05-29 22:13:27 +0000

    getFormEditorConfig() should return the config interface

--- modeling/plugins/org.eclipse.pde.emfforms/src/org/eclipse/pde/emfforms/editor/EmfFormEditor.java
+++ modeling/plugins/org.eclipse.pde.emfforms/src/org/eclipse/pde/emfforms/editor/EmfFormEditor.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Anyware Technologies - initial API and implementation
  *
- * $Id: EmfFormEditor.java,v 1.4 2009/02/27 09:26:33 bcabe Exp $
+ * $Id: EmfFormEditor.java,v 1.5 2009/04/24 11:52:09 bcabe Exp $
  */
 package org.eclipse.pde.emfforms.editor;
 
@@ -118,10 +118,9 @@ public abstract class EmfFormEditor<T extends EObject> extends FormEditor implem
 	}
 
 	/**
-	 * 
-	 * @return
+	 * @return the {@link IEmfFormEditorConfig} the editor will use as its configuration
 	 */
-	protected DefaultEmfFormEditorConfig getFormEditorConfig() {
+	protected IEmfFormEditorConfig getFormEditorConfig() {
 		return new DefaultEmfFormEditorConfig();
 	}
 
