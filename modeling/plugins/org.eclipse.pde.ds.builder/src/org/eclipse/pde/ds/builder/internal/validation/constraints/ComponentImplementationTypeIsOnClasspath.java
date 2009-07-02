bb9967f8f5193d93b16f58abad4f73b2a97f8b33
Benjamin Cabe <bcabe>
2009-07-02 21:54:54 +0000

    ongoing builder improvements : validate only component models referenced in the Service-Component header of the bundle's manifest

--- modeling/plugins/org.eclipse.pde.ds.builder/src/org/eclipse/pde/ds/builder/internal/validation/constraints/ComponentImplementationTypeIsOnClasspath.java
+++ modeling/plugins/org.eclipse.pde.ds.builder/src/org/eclipse/pde/ds/builder/internal/validation/constraints/ComponentImplementationTypeIsOnClasspath.java
@@ -8,9 +8,9 @@
  * Contributors:
  *     Anyware Technologies - initial API and implementation
  *
- * $Id: OverviewPage.java,v 1.9 2009/06/02 10:48:05 bcabe Exp $
+ * $Id: ComponentImplementationTypeIsOnClasspath.java,v 1.2 2009/07/02 09:34:02 bcabe Exp $
  */
- package org.eclipse.pde.ds.builder.internal.validation.constraints;
+package org.eclipse.pde.ds.builder.internal.validation.constraints;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IStatus;
@@ -38,7 +38,7 @@ public class ComponentImplementationTypeIsOnClasspath extends
 			type = project.findType(impl.getClass_());
 			if (type == null || !type.exists()) {
 				ConstraintStatus s = (ConstraintStatus) ctx
-						.createFailureStatus();
+						.createFailureStatus(impl.getClass_());
 				EnhancedConstraintStatus enhancedStatus = new EnhancedConstraintStatus(
 						s, ScrPackage.Literals.IMPLEMENTATION__CLASS);
 				return enhancedStatus;
