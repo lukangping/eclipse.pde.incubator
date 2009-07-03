9a07e0059c17fdccf94e8e1e13ec6d240255dbe8
Benjamin Cabe <bcabe>
2009-07-03 20:59:37 +0000

    perform validation on file refresh

--- modeling/plugins/org.eclipse.pde.ds.ui/src/org/eclipse/pde/ds/ui/internal/editor/DSEditor.java
+++ modeling/plugins/org.eclipse.pde.ds.ui/src/org/eclipse/pde/ds/ui/internal/editor/DSEditor.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Anyware Technologies - initial API and implementation
  *
- * $Id: DSEditor.java,v 1.6 2009/04/24 21:52:48 bcabe Exp $
+ * $Id: DSEditor.java,v 1.7 2009/06/02 09:09:08 bcabe Exp $
  */
 package org.eclipse.pde.ds.ui.internal.editor;
 
@@ -89,6 +89,7 @@ public class DSEditor extends EmfFormEditor<Component> implements IResourceChang
 									// current object
 									if (isMainResource) {
 										setMainResource(changedResource);
+										getDataBindingContext().updateModels();
 									}
 								} catch (IOException ioe) {
 									Activator.log(ioe);
