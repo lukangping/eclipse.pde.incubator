f2be333455b8513eb4e1fe137170aa5221f911ba
Benjamin Cabe <bcabe>
2009-09-13 19:50:28 +0000

    doc

--- modeling/plugins/org.eclipse.pde.emfforms/src/org/eclipse/pde/emfforms/editor/DefaultEmfFormEditorConfig.java
+++ modeling/plugins/org.eclipse.pde.emfforms/src/org/eclipse/pde/emfforms/editor/DefaultEmfFormEditorConfig.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Anyware Technologies - initial API and implementation
  *
- * $Id: DefaultEmfFormEditorConfig.java,v 1.5 2009/09/13 18:04:04 bcabe Exp $
+ * $Id: DefaultEmfFormEditorConfig.java,v 1.6 2009/09/13 18:18:43 bcabe Exp $
  */
 package org.eclipse.pde.emfforms.editor;
 
@@ -58,6 +58,9 @@ public class DefaultEmfFormEditorConfig<E extends EmfFormEditor<O>, O extends EO
 		this.customizedToolkit = customizedToolkit;
 	}
 
+	/**
+	 * This default implementation returns the eResource of suggestedInput
+	 */
 	public Object getOutlineInput(O suggestedInput) {
 		return suggestedInput.eResource();
 	};
